9962fb816dc2d0602dbeddb020fa79db04d7a3b1
MichaelNewham <mail2mick@gmail.com>
2021-04-16 11:25:35 +0000

    Fav Icon: Added this to repo and changed index.html title to Love Maths!

--- assets/jsfiles/script.js
+++ assets/jsfiles/script.js
@@ -105,6 +105,9 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "X";
 }
 
-function displayDivideQuestion() {
+function displayDivideQuestion(operand1, operand2) {
 
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "/";
 }
